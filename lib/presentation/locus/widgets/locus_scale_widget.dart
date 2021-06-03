d563f31502fe62a2af928cd035d26c43a58c6fd7
oberdancunha <oberdancunha@gmail.com>
2021-06-03 15:22:21 -0300

    Refactoring locus length scale.

--- lib/presentation/locus/widgets/locus_scale_widget.dart
+++ lib/presentation/locus/widgets/locus_scale_widget.dart
@@ -12,11 +12,32 @@ class LocusScaleWidget extends StatelessWidget {
         builder: (context, state) {
           final screenSize = MediaQuery.of(context).size.width;
           final locusLength = state.locusShowed.length;
-          final scaleByLocusLengthCharacters = locusLength.toString().length / 2;
+          // final locusLength = 1234567;
+          final double? scalingFactor;
+          if (locusLength > 0 && locusLength <= 99999) {
+            scalingFactor = 2;
+          } else if (locusLength > 100000 && locusLength <= 999999) {
+            scalingFactor = 1.8;
+          } else if (locusLength > 1000000 && locusLength <= 9999999) {
+            scalingFactor = 1.7;
+          } else if (locusLength > 10000000 && locusLength <= 99999999) {
+            scalingFactor = 1.5;
+          } else if (locusLength > 100000000 && locusLength <= 999999999) {
+            scalingFactor = 1.4;
+          } else {
+            scalingFactor = 1;
+          }
+          final scaleByLocusLengthCharacters = locusLength.toString().length / scalingFactor;
+          final markingPoints = 5 * (pow(10, scaleByLocusLengthCharacters)).round();
           final scale = (screenSize / locusLength) * scaleByLocusLengthCharacters;
-          final normalizeScale = 5 * (pow(10, scaleByLocusLengthCharacters)).round();
           final screenWidthScale = locusLength * scale;
-          final locusLengthScale = (locusLength * scale).floor();
+          final locusLengthScale = (locusLength * scale).round();
+
+          debugPrint(scale.toString());
+          debugPrint(scaleByLocusLengthCharacters.toString());
+          debugPrint(screenWidthScale.toString());
+          debugPrint(locusLengthScale.toString());
+          debugPrint(markingPoints.toString());
 
           return SingleChildScrollView(
             scrollDirection: Axis.horizontal,
@@ -27,27 +48,13 @@ class LocusScaleWidget extends StatelessWidget {
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(10),
               ),
-              child: Stack(
-                children: [
-                  CustomPaint(
-                    painter: PainterLocusScale(
-                      drawType: 'line',
-                      width: double.tryParse(locusLengthScale.toString())!,
-                      locusLength: locusLength,
-                    ),
-                  ),
-                  for (int markerScale = normalizeScale;
-                      markerScale < (state.locusShowed.length - normalizeScale);
-                      markerScale += normalizeScale)
-                    CustomPaint(
-                      painter: PainterLocusScale(
-                        drawType: 'marker',
-                        width: 30,
-                        locusLength: markerScale,
-                        scale: scale,
-                      ),
-                    ),
-                ],
+              child: CustomPaint(
+                painter: PainterLocusScale(
+                  width: double.tryParse(locusLengthScale.toString())!,
+                  locusLength: locusLength,
+                  scale: scale,
+                  markingPoints: markingPoints,
+                ),
               ),
             ),
           );
@@ -56,16 +63,16 @@ class LocusScaleWidget extends StatelessWidget {
 }
 
 class PainterLocusScale extends CustomPainter {
-  final String drawType;
   final double width;
   final int locusLength;
-  final double? scale;
+  final double scale;
+  final int markingPoints;
 
   PainterLocusScale({
-    required this.drawType,
     required this.width,
     required this.locusLength,
-    this.scale,
+    required this.scale,
+    required this.markingPoints,
   });
 
   @override
@@ -73,24 +80,34 @@ class PainterLocusScale extends CustomPainter {
     final paint = Paint()
       ..color = Colors.black
       ..strokeWidth = 1;
+    _drawLine(canvas, paint);
+    _drawMarker(canvas, paint);
+  }
 
-    if (drawType == 'line') {
-      final rightSizeAndAngle = Offset(width, 40);
-      const leftSizeAndAngle = Offset(10, 40);
-      canvas.drawLine(rightSizeAndAngle, leftSizeAndAngle, paint);
-      _paintCanvas(
-        canvas: canvas,
-        text: '1',
-        textAlign: 10,
-      );
-      _paintCanvas(
-        canvas: canvas,
-        text: locusLength.toString(),
-        textAlign: width - (locusLength.toString().length * 10),
-      );
-    } else if (drawType == 'marker') {
-      final marker = double.tryParse(locusLength.toString());
-      final markerScale = marker! * scale!;
+  void _drawLine(Canvas canvas, Paint paint) {
+    final rightSizeAndAngle = Offset(width, 40);
+    const leftSizeAndAngle = Offset(10, 40);
+    canvas.drawLine(rightSizeAndAngle, leftSizeAndAngle, paint);
+    _paintCanvas(
+      canvas: canvas,
+      text: '1',
+      textAlign: 10,
+    );
+    _paintCanvas(
+      canvas: canvas,
+      text: locusLength.toString(),
+      textAlign: width - (locusLength.toString().length * 10),
+    );
+  }
+
+  // 254.58
+  // 83.4
+
+  void _drawMarker(Canvas canvas, Paint paint) {
+    for (int marker = markingPoints;
+        marker < (locusLength - markingPoints);
+        marker += markingPoints) {
+      final markerScale = marker * scale;
       final topMarker = Offset(markerScale, 35);
       final bottomMarker = Offset(markerScale, 45);
       canvas.drawLine(topMarker, bottomMarker, paint);
